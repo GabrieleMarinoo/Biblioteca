9f1e62e7781b7a316aa24eaf23ecd308fe665a52
GabrieleMarinoo <99045944+GabrieleMarinoo@users.noreply.github.com>
2023-09-22 12:44:18 +0200

    Merge branch 'master' of https://github.com/GabrieleMarinoo/Biblioteca.git

---
+++ src/main/java/it/esinware/biblioteca/Biblioteca.java
@@ -0,0 +1,26 @@
+package it.esinware.biblioteca;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import ch.qos.logback.classic.Logger;
+
+@SpringBootApplication
+@EnableWebMvc
+public class Biblioteca {
+	
+	 private static final Logger logger 
+     = (Logger) LoggerFactory.getLogger(Biblioteca.class);
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(Biblioteca.class);
+		app.addListeners(new ApplicationPidFileWriter("biblioteca.pid"));
+		app.run(args);
+		logger.info("Esempio di logging", Biblioteca.class.getSimpleName());
+		
+	}
+
+}
